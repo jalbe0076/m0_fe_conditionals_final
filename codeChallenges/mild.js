056dbddbe80b95703ad91d37e126ea415ee679d2
Jason Alberto <alberto.jason@gmail.com>
2023-02-08 20:47:19 -0500

    Complete mild challenge

---
+++ codeChallenges/mild.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program that defines a variable that stores a Number. 
+//The program should print out the String "even" if the Number is even, and the String "odd" if the Number is odd. 
+//Hint: You may need to do some extra research on the remainder operator (%) to solve this challenge.
+
+var myNumber = 4324323;
+
+if (myNumber % 2 === 0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
